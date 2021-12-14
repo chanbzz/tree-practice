16817da1f0d895db09699462babaf4ea8e53454d
chanbzz <cboswell1024@gmail.com>
2021-12-14 18:57:11 -0500

    bst in python

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -1,5 +1,5 @@
 class TreeNode:
-    def __init__(self, key, val = None):
+    def __init__(self, key, val=None):
         if val == None:
             val = key
 
@@ -17,18 +17,48 @@ class Tree:
     # Time Complexity: 
     # Space Complexity: 
     def add(self, key, value = None):
-        pass
+        if self.root == None:
+            tree = TreeNode(key, value)
+        
+            
+
+            
+
 
     # Time Complexity: 
     # Space Complexity: 
     def find(self, key):
-        pass
+        if self.root == None:
+            return None
+        else:
+            current = self.root
+
+            while current:
+                if current.key == key:
+                    return current.value
+                elif current.key < key:
+                    current = current.right
+                else:
+                    current = current.left
+
 
     # Time Complexity: 
     # Space Complexity: 
     def inorder(self):
-        pass
+        if self.root == None:
+            return []
+        else:
+            def order_nodes(current_node, array):
+                if current_node:
+                    self.order_nodes(current_node.left, array)
+                    array.append(key=current_node.key, value=current_node.value)
+                    self.order_nodes(current_node.right, array)           
 
+            ordered_list = []
+            current = self.root
+            return order_nodes(current, ordered_list)
+        
+        
     # Time Complexity: 
     # Space Complexity:     
     def preorder(self):
